c44fdbb198fb23f09034da928fd995e40d7727ff
CARLOS REINALDO GARCIA <kalliche16@gmail.com>
2023-07-18 04:43:01 -0500

    montar server

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+# Creamos una instancia de la libreria
+app = FastAPI()
+
+# agragamos un decorador o especificamos la ruta
+@app.get('/')   # ruta principal
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get('/contact', response_class=HTMLResponse)    # ruta de contqcto
+def get_list():
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>Soy un parerafo</p>
+    """
 
 def run():
     store.get_categories()
